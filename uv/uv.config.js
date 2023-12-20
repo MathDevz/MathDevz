f545229366268d56a39d831e8ab389757302b6b0
ProGrammer_365 <133169817+ProCoder36908@users.noreply.github.com>
2023-12-20 18:13:51 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/service/',
+    prefix: '/mathhelp/',
     bare: 'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
